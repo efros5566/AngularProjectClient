0cbaa7c6f74b803e8af69515dbde549c95af47c7
efros5566 <148768594+efros5566@users.noreply.github.com>
2024-03-10 03:41:40 +0200

    so late

--- AngularProject/src/app/all-recipes/all-recipes.component.ts
+++ AngularProject/src/app/all-recipes/all-recipes.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-all-recipes',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './all-recipes.component.html',
   styleUrl: './all-recipes.component.css'
 })
 export class AllRecipesComponent {
-
+constructor(private router: Router) {
+}
 }
--- AngularProject/src/app/login/login.component.ts
+++ AngularProject/src/app/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports:[RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  constructor(private router: Router) { }
 }
